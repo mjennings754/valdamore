cd261929b47921775956543d4ccaa3649303906e
mjennings754 <mitchellj@outlook.jp>
2026-02-11 18:07:08 +1100

    add game loop with options

--- main.py
+++ main.py
@@ -10,10 +10,32 @@ player = Player(username)
 
 print(f"{player.username} begins their journey in Cambridge")
 sleep(1)
-print(cow1)
-userinput = int(input("1) Attack the cow 2) Leave the cow alone \n"))
-if userinput == 1:
-    print("You attack the cow")
-    cow1.attack(player)
-else:
-    print("You let the cow chew on some grass!")
+while True:
+    print("\nWhat would you like to do?")
+    print("1) Adventure around")
+    print("2) Go to cow area")
+    print("3) Check stats")
+    print("4) Quit")
+
+    userinput = int(input("> "))
+
+    if userinput == 1:
+        print("You go adventure around Valdamore!")
+    elif userinput == 2:
+        print("You see a cow")
+        userinput = int(input("1) Attack the cow 2) Leave the cow alone \n"))
+        if userinput == 1:
+            cow1.attack(player)
+        else:
+            print("The cow continues to eat grass")
+
+    elif userinput == 3:
+        print(f"Attack: {player.skills['Attack']}")
+        print(f"Attack XP: {player.experience['Attack']}")
+
+    elif userinput == 4:
+        print("Goodbye!")
+        break
+
+    else:
+        print("invalid option")
